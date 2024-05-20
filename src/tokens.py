7a56918fe079ddf25b81dff095e281ad03dee424
Jack Spencer <UniformKitten281@gmail.com>
2024-05-20 08:41:41 -0700

    Create tokens.py

---
+++ src/tokens.py
@@ -0,0 +1,54 @@
+tokens = {
+    'LEFT_PAREN': '(',
+    'RIGHT_PAREN': ')',
+    'LEFT_BRACE': '{',
+    'RIGHT_BRACE': '}',
+    'LEFT_BRACKET': '[',
+    'RIGHT_BRACKET': ']',
+    'COMMA': ',',
+    'PERIOD': '.',
+    'PLUS': '+',
+    'MINUS': '-',
+    'ASTERISK': '*',
+    'SLASH': '/',
+    'AND': '&&',
+    'OR': '||',
+    'BANG': '!',
+    'EQUAL': '=',
+    'EQUAL_EQUAL': '==',
+    'BANG_EQUAL': '!=',
+    'GREATER': '>',
+    'GREATER_EQUAL': '>=',
+    'LESS': '<',
+    'LESS_EQUAL': '<=',
+    'EOF': '\0',
+    'SINGLE_QUOTE': "'",
+    'DOUBLE_QUOTE': '"'
+}
+
+keywords = {
+    'STRING': 'string',                 # Data types
+    'INTEGER': 'integer',               # Data types
+    'CHARACTER': 'character',           # Data types
+    'DECIMAL': 'decimal',               # Data types 
+    'BOOLEAN': 'boolean',               # Data types
+    'UNDEFINED': 'undefined',           # Data types
+    'FINAL': 'final',                   # Variables
+    'STATIC': 'static',                 # Variables
+    'TRUE': 'true',                     # Boolean
+    'FALSE': 'false',                   # Boolean
+    'IF': 'if',                         # Conditional
+    'ELSE': 'else',                     # Conditional
+    'WHILETRUE': 'whiletrue',           # Loop
+    'WHILEFALSE': 'whilefalse',         # Loop
+    'WHILEUNDEFINED': 'whileundefined', # Loop
+    'FOR': 'for',                       # Loop
+    'SUPER': 'super',                   # Function
+    'SWITCH': 'switch',                 # Conditional
+    'CLASS': 'class',                   # Class
+    'RETURN': 'return',                 # Function
+    'THIS': 'this',                     # Function
+
+    'BRIDGE': 'bridge',                 # File Exporting
+    'LINK': 'link'                      # File Importing
+}
